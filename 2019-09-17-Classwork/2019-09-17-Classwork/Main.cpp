7246b7fdf1a465d6b95bf9c60447fbd4811a96dd
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-09-17 08:43:38 -0400

    Add files via upload

---
+++ 2019-09-17-Classwork/2019-09-17-Classwork/Main.cpp
@@ -0,0 +1,12 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-17-Classwork
+
+#include <iostream>
+
+int main()
+{
+
+}
---
+++ Class Work 1/Class Work 1/Class Work 1.cpp
@@ -0,0 +1,18 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr.Williams
+// Classwork1
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "O'Ryan\n";
+	std::cout << "O'Ryan\n";
+	std::cout << "O'Ryan\n";
+	std::cout << "O'Ryan\n";
+	std::cout << "O'Ryan\n";
+}
+
+
