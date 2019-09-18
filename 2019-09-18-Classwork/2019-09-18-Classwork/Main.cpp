7dcad08795aa1a5f6972b30be195cb8272e3999d
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-09-18 08:22:20 -0400

    Add files via upload

---
+++ 2019-09-18-Classwork/2019-09-18-Classwork/Main.cpp
@@ -0,0 +1,10 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr.Williams
+// 2019-09-18-Classwork
+#include <iostream>
+int main()
+{
+	std::cout << "what's poppin" << std::endl;
+}
