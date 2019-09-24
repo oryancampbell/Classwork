f79fc115e5ec252ac07682b1bcae804f22e2126c
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-09-24 08:37:34 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/Main.cpp
@@ -0,0 +1,25 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-09-24-Classwork
+
+#include <iostream>
+
+int main()
+{
+
+	int jamIsJam = 9;
+	int j = 20;
+	int a = 14;
+	int m = 5;
+	int i = 7;
+	int g = 1;
+	int k = 6;
+	int p = 4;
+	int d = 0;
+	int e = 8;
+	std::cout << sizeof(jamIsJam) << std::endl;
+
+}
+
