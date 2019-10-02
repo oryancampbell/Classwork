d53ec518e9ab2978c9e6d31c146531644c259b87
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-10-02 08:39:57 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/Main.cpp
@@ -0,0 +1,24 @@
+// Name: O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-10-02
+
+#include <iostream> 
+int main()
+{
+	char K = 'A';
+	char Z = 'B';
+	char R = 'J';
+	char F = 'P';
+	int Health = 0;
+	int Basketball = 100;
+	int Minecraft = 50;
+	bool playerIsDying = true;
+	bool playerIsDrowning = true;
+	bool playerIsDoingFine = false;
+	float Dragonball = 9999;
+	float Naruto = 1000000;
+	float onePiece = 20000000;
+
+}
