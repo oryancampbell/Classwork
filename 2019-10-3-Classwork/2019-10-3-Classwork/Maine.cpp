a8083b44b866f7d05789be43a65567c0db655e81
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2019-10-03 08:40:47 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/Maine.cpp
@@ -0,0 +1,20 @@
+// Name O'Ryan Campbell
+// Intro to Computer Science
+// Period 1
+// Mr. Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite number:";
+	int favoriteNumber;
+	cin >> favoriteNumber;
+	cout << "Cool," << favoriteNumber << " is my favorite number too!" << std::endl;
+	cout << "Enter the length of a side of a square";
+	int sideLength;
+	cin >> sideLength;
+	cout << "Perimeter = 20cm," << sideLength << " Area = 25cm" << std::endl;
+}
